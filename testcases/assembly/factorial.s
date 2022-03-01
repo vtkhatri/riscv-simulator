d3035a733e82306963c2ccc469095e92a48dabea
vtkhatri <virajkhatri@gmail.com>
2022-02-28 18:15:37 -0800

    factorial testcase

---
+++ testcases/assembly/factorial.s
@@ -0,0 +1,25 @@
+factorial:
+    addi sp, sp, -8 # save regs
+    sw a0, 4( sp )
+    sw ra, 0( sp )
+    addi t0, zero, 1 # temporary = 1
+    bgt a0, t0, else # if n>1, go to else
+    addi a0, zero, 1 # otherwise, return 1
+    addi sp , sp , 8 # restore sp
+    jr ra # return
+else:
+    addi a0, a0, -1 # n = n − 1
+    jal factorial # recursive call
+    lw t1, 4( sp ) # restore n into t1
+    lw ra, 0( sp ) # restore ra
+    addi sp, sp, 8 # restore sp
+    mul a0, t1, a0 # a0=n*factorial(n−1)
+    jr ra # return
+main:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    addi a0, zero, 6 # !6 = 720
+    jal factorial
+    addi sp, sp, 4
+    lw ra, -4(sp)
+    jr ra
